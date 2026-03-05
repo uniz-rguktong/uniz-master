3a6e9a72502570b5441151dd021d490bbc99643f
sreecharan-desu <sreecharan309@gmail.com>
2026-03-05 18:14:21 +0530

    chore: deployment update 2026-03-05 18:14:21

--- apps/uniz-academics/src/index.ts
+++ apps/uniz-academics/src/index.ts
@@ -14,7 +14,8 @@ app.use(helmet());
 app.use(compression());
 // app.use(cors());
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Attribution & Malformed Activity Handling (Mandatory)
 import { attributionMiddleware } from "./middlewares/attribution.middleware";
--- apps/uniz-gateway/src/index.ts
+++ apps/uniz-gateway/src/index.ts
@@ -44,7 +44,8 @@ app.use((req, res, next) => {
 
   next();
 });
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 const services = [
   {
